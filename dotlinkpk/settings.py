7fe54d02198f0309fd9402923256c701d52c8fc5
Muhammad Hanif <m.haneef1966@gmail.com>
2023-04-02 17:32:05 +0200

    add db path

--- dotlinkpk/settings.py
+++ dotlinkpk/settings.py
@@ -75,14 +75,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dotlinkpk.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+sqllight_path = os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': sqllight_path
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
